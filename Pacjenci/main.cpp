92a51b5c90a900de68bae23b4f83f7f4394050d9
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2024-05-21 18:40:34 +0200

    naprawiono polskie znaki

--- Pacjenci/main.cpp
+++ Pacjenci/main.cpp
@@ -1,11 +1,10 @@
 #include "Pacjenci.h"
 #include <QtWidgets/QApplication>
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Pacjenci w;
-    w.setWindowState(Qt::WindowMaximized);
+    //w.setWindowState(Qt::WindowMaximized);
     w.show();
     return a.exec();
 }
